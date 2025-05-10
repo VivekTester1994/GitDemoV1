14d89abd14ca2cbbe60a70eaa159c8fb1cdbbbb7
vivek <vivekjacob1994@hotmail.com>
2025-05-10 12:24:46 +0530

    MyFirstCommit

---
+++ src/Package1/Lesson1.java
@@ -0,0 +1,16 @@
+package Package1;
+
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="vivek";
+		if(name=="vivek")
+			System.out.println("thgis is my name");
+		else {
+			System.out.println("this is not my name");
+		}
+	
+	}
+
+}
